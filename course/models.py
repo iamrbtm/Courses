607c932efbb846094d15043999a3d06d796f2133
Jeremy Guill <rbtm2006@me.com>
2022-02-08 00:38:24 -0800

    first commit

---
+++ course/models.py
@@ -0,0 +1,37 @@
+from new import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+import datetime, os
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy import ForeignKey
+
+
+# Users
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(150))
+    lastname = db.Column(db.String(150))
+    address = db.Column(db.String(150))
+    city = db.Column(db.String(150))
+    state = db.Column(db.String(2))
+    zip = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+    email = db.Column(db.String(150), unique=True)
+    dob = db.Column(db.Date)
+    avatar_filename = db.Column(db.Text)
+    avatar_url = db.Column(db.String(250))
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+
+class States(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(50))
+    abr = db.Column(db.String(2))
+
+# class <name>(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+
+#     userid = db.Column(db.Integer)
+#     update_time = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+#     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
